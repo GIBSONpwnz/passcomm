16063e590cabea4f103b8396ba4787f038dc50b3
GIBSONpwnz <45794576+GIBSONpwnz@users.noreply.github.com>
2019-02-11 11:23:07 +0300

    Update index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<div id="header">
+<h1 class="site-title">Site Title</h1>
+<div class="nav-menu"></div>
+</div>
+<div id="main">
+<div class="post"></div>
+</div>
+<div id="sidebar">
+<div class="widget"></div>
+</div>
+<div id="footer"></div>
+</body>
+</html>
